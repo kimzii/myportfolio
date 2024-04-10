333e3927bc9c8e89b5e22623028c49b98de32d92
kimzii <kimzietorres@gmail.com>
2024-04-10 08:05:35 +0800

    removed unused img, changed experience layout

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -6,26 +6,28 @@ export const Experience = () => {
     <section className={styles.experience} id="experience">
         <h2>EXPERIENCE</h2>
 
-        <div className={styles.expctn}>
-            <img src={GDG} alt="" width="400px"/>
-            <h4>Partnership Manager</h4>
-            <span>8 months</span>
-            <span>Google Developer Student Clubs USeP Obrero</span>
-        </div>
+        <div>
+            <div className={styles.expctn}>
+                <img src={GDG} alt="" width="320px"/>
+                <h4>Partnership Manager</h4>
+                <span>8 months</span>
+                <span>Google Developer Student Clubs USeP Obrero</span>
+            </div>
 
-        <div className={styles.expctn}>
-            <img src={GDG} alt="" width="400px"/>
-            <h4>Logistics Head</h4>
-            <span>1 month</span>
-            <span>Google Developer Group Davao</span>
-        </div>
+            <div className={styles.expctn}>
+                <img src={GDG} alt="" width="320px"/>
+                <h4>Logistics Head</h4>
+                <span>1 month</span>
+                <span>Google Developer Group Davao</span>
+            </div>
 
-        <div className={styles.expctn}>
-            <img src={GDG} alt="" width="400px"/>
-            <h4>Student Developer</h4>
-            <span>2 years</span>
-            <span>University of Southeastern Philippines</span>
-        </div>
-    </section>
+            <div className={styles.expctn}>
+                <img src={GDG} alt="" width="320px"/>
+                <h4>Student Developer</h4>
+                <span>2 years</span>
+                <span>University of Southeastern Philippines</span>
+            </div>
+          </div>
+      </section>
   )
 }
